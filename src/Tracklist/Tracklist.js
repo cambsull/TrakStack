75d688c04132fa23cda112db6e72c2c1464154c5
Cambria Sullivan <72573258+cambsull@users.noreply.github.com>
2023-10-11 15:44:43 -0700

    Added comments to playlist saving functionality, added alert to indicate successful save.

--- src/Tracklist/Tracklist.js
+++ src/Tracklist/Tracklist.js
@@ -6,8 +6,9 @@ import Track from '../Track/Track.js';
 function Tracklist(props) {
     const [playlistName, setPlaylistName] = useState(''); 
     const btnRef = useRef(null);
-    const { token } = props;
+    const { token } = props; //Destructure token from App.js
 
+    //Helper function to create new playlist
     const createSpotifyPlaylist = (userId, name, token) => {
         return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
             method: 'POST',
@@ -15,43 +16,45 @@ function Tracklist(props) {
                 'Authorization': `Bearer ${token}`,
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ name }),
+            body: JSON.stringify({ name }), //Stringify the playlist name for click handler
         })
-        .then(response => response.json())
-        .then(data => data.id);  // Returning the new playlist's id.
+        .then(response => response.json()) //Convert response to JSON
+        .then(data => data.id);  // Return the id specifically from the data return.
     };
 
-    const addTracksToPlaylist = (playlistId, tracks, token) => {
-        const trackUris = tracks.map(track => `spotify:track:${track.id}`);
+    //Helper function to add tracks to playlist when it is saved
+    const addTracksToPlaylist = (playlistId, tracks, token) => { //Takes the playlist ID, tracks (from search in App.js), and user token
+        const trackUris = tracks.map(track => `spotify:track:${track.id}`); //Create a map of track URIs from tracks passed in from search results
         return fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
             method: 'POST',
             headers: { 
                 'Authorization': `Bearer ${token}`,
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ uris: trackUris })
+            body: JSON.stringify({ uris: trackUris }) //Stringify the URIs for use in next API call
         })
-        .then(response => response.json()); 
+        .then(response => response.json());  //Convert to JSON
     };
 
     const handleClick = () => {
-        const finalPlaylistName = playlistName || 'My Playlist';
+        const finalPlaylistName = playlistName || 'My Playlist'; //Take the playlist name from the form entry, or use 'My Playlist' as a default
 
         if (token) {  
             fetch('https://api.spotify.com/v1/me', {
                 method: 'GET',
                 headers: { 'Authorization': `Bearer ${token}` },
             })
-            .then(response => response.json())
-            .then(data => createSpotifyPlaylist(data.id, finalPlaylistName, token))
-            .then(playlistId => addTracksToPlaylist(playlistId, props.tracks, token))
-            .catch(error => console.error('Error: ', error));
+            .then(response => response.json()) //Convert to JSON
+            .then(data => createSpotifyPlaylist(data.id, finalPlaylistName, token)) //Take the data, pass it to createSpotifyPlaylist
+            .then(playlistId => addTracksToPlaylist(playlistId, props.tracks, token)) //Take that data, which creates the playlist ID, and pass it to addTracksToPlaylist
+            .then(alert("Playlist saved successfully!"))
+            .catch(error => console.error('Error: ', error)); //In case there's an oopsie
         } else {
             console.error('No token available');
         }
 
         if (btnRef.current) {
-            btnRef.current.classList.add(styles.flash); 
+            btnRef.current.classList.add(styles.flash); //Golden animation
             setTimeout(() => {
                 btnRef.current.classList.remove(styles.flash);
             }, 500);
@@ -65,7 +68,7 @@ function Tracklist(props) {
     return (
         <>
             <div>
-                <form className={styles.playlistName} onSubmit={e => e.preventDefault()}>
+                <form className={styles.playlistName} onSubmit={e => e.preventDefault()}> 
                     <input 
                         className={styles.playlistName} 
                         placeholder='Playlist name...' 
