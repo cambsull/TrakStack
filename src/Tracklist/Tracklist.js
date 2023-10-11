e258490a1bbeda9a12987078398eb1b03d4710c2
Cambria Sullivan <72573258+cambsull@users.noreply.github.com>
2023-10-11 15:27:03 -0700

    Added logic to save playlists

--- src/Tracklist/Tracklist.js
+++ src/Tracklist/Tracklist.js
@@ -1,74 +1,90 @@
-//Tracklist Component
-import React, { useRef } from 'react';
+//Tracklist component
+import React, { useRef, useState } from 'react';
 import styles from './Tracklist.module.css';
 import Track from '../Track/Track.js';
 
 function Tracklist(props) {
-    //Obtain access token from App.js via destructuring.
+    const [playlistName, setPlaylistName] = useState(''); 
+    const btnRef = useRef(null);
     const { token } = props;
 
-    //Save to spotify button logic and flash animation
-    const btnRef = useRef(null);
+    const createSpotifyPlaylist = (userId, name, token) => {
+        return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ name }),
+        })
+        .then(response => response.json())
+        .then(data => data.id);  // Returning the new playlist's id.
+    };
+
+    const addTracksToPlaylist = (playlistId, tracks, token) => {
+        const trackUris = tracks.map(track => `spotify:track:${track.id}`);
+        return fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
+            method: 'POST',
+            headers: { 
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ uris: trackUris })
+        })
+        .then(response => response.json()); 
+    };
 
     const handleClick = () => {
-        if (btnRef.current) {
-            if (token) {  // Using token in Tracklist
-                // Perform GET request with token to get user ID
-                fetch('https://api.spotify.com/v1/me', {
-                    method: 'GET',
-                    headers: {
-                        'Authorization': `Bearer ${token}`,
-                    },
-                })
-                    .then(response => response.json())
-                    .then(data => {
-                        console.log('User ID: ', data.id);  // Logging the User ID
-                    })
-                    .catch(error => {
-                        console.error('Error: ', error);
-                    });
-            } else {
-                console.error('No token available');
-            }
+        const finalPlaylistName = playlistName || 'My Playlist';
+
+        if (token) {  
+            fetch('https://api.spotify.com/v1/me', {
+                method: 'GET',
+                headers: { 'Authorization': `Bearer ${token}` },
+            })
+            .then(response => response.json())
+            .then(data => createSpotifyPlaylist(data.id, finalPlaylistName, token))
+            .then(playlistId => addTracksToPlaylist(playlistId, props.tracks, token))
+            .catch(error => console.error('Error: ', error));
+        } else {
+            console.error('No token available');
         }
 
         if (btnRef.current) {
-            btnRef.current.classList.add(styles.flash); // Apply the animation
-
-            // Remove the animation class after 0.5 seconds (the duration of our animation)
+            btnRef.current.classList.add(styles.flash); 
             setTimeout(() => {
                 btnRef.current.classList.remove(styles.flash);
             }, 500);
         }
     };
 
-
-
-
-    //Ability to remove tracks from playlists
     const removeTrack = (trackId) => {
         props.setTracks(prevTracks => prevTracks.filter(track => track.id !== trackId));
     };
 
-
     return (
         <>
             <div>
-                <form className={styles.playlistName}>
-                    <input className={styles.playlistName} placeholder='Playlist name...'></input>
+                <form className={styles.playlistName} onSubmit={e => e.preventDefault()}>
+                    <input 
+                        className={styles.playlistName} 
+                        placeholder='Playlist name...' 
+                        onChange={(e) => setPlaylistName(e.target.value)}
+                        value={playlistName}
+                    />
                 </form>
             </div>
             <div className="tracklist">
-                {props.tracks.map((track) => <Track key={track.id} {...track} onRemove={removeTrack} /> //Map Track props to each track, spread operator to bring entire data structure along
+                {props.tracks.map((track) => 
+                    <Track key={track.id} {...track} onRemove={removeTrack} />
                 )}
             </div>
             <button className={styles.submitButton} onClick={handleClick} ref={btnRef}>
                 <span className={styles.desktopText}>Save your playlist to your Spotify account!</span>
                 <span className={styles.mobileText}>Save to Spotify!</span>
             </button>
-
         </>
-    )
+    );
 }
 
 export default Tracklist;
